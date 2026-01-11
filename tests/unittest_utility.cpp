f117d1f829025bbd8a97fae849d7ee34bed6be44
Paul Kim <pauljoohyunkim@gmail.com>
2026-01-11 15:59:55 +0900

    Entanglement test 3

--- tests/unittest_utility.cpp
+++ tests/unittest_utility.cpp
@@ -19,3 +19,26 @@ TEST(Utility, Entangle2) {
 
     ASSERT_EQ(sv.size(), 1 << (2 * 3));
 }
+
+TEST(Utility, Entangle3) {
+    const double alpha { 0.2 };
+    const double beta { std::sqrt(1-alpha*alpha) };
+    const double phi { 0.3 };
+    const std::complex<double> j { 0, 1 };
+    const auto phaseShift { std::exp(j * phi) };
+    Solace::Qubits q0 { alpha, -phaseShift * std::conj(beta) };
+    Solace::Qubits q1 { beta, phaseShift * std::conj(alpha) };
+
+    Solace::Qubits q2 { q0 ^ q1 };
+
+    // Expected probability of the entangled states.
+    Solace::StateVector expectedSv(4);
+    expectedSv << alpha * beta,
+                  alpha * phaseShift * std::conj(alpha),
+                  -phaseShift * std::conj(beta) * beta,
+                  -std::conj(alpha * beta) * phaseShift * phaseShift;
+    Solace::StateVector sv { q2.viewStateVector() };
+    Solace::StateVector diff = sv - expectedSv;
+
+    ASSERT_TRUE(std::abs(diff.norm()) < 0.0001);
+}
