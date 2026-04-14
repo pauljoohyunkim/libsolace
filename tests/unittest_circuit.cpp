01c01135f0d7b771088f36e86cf8dc2ca40508c9
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-04-14 19:35:41 +0900

    viewTransformer -> getTransformer where it is no longer blocked by BE_A_QUAUNTUM_CHEATER flag

--- tests/unittest_circuit.cpp
+++ tests/unittest_circuit.cpp
@@ -14,14 +14,14 @@ TEST(CircuitTest, AddQuantumGate) {
     Solace::Gate::Hadamard H {};
     auto H0 { qc.addQuantumGate(H) };
     std::cout << "QuantumGate Reference: " << (int) H0 << std::endl;
-    //auto tMaybe { H0->viewTransformer() };
+    //auto tMaybe { H0->getTransformer() };
     //auto t { std::get<Solace::QuantumGateTransformer>(tMaybe) };
 }
 
 TEST(CircuitTest, ApplyQuantumGateToQubits) {
     Solace::QuantumCircuit qc;
     auto H { qc.addQuantumGate(Solace::Gate::Hadamard()) };
-    //auto tMaybe { H->viewTransformer() };
+    //auto tMaybe { H->getTransformer() };
     //auto t { std::get<Solace::QuantumGateTransformer>(tMaybe) };
 
     // Create a single qubit on the circuit.
@@ -41,7 +41,7 @@ TEST(CircuitTest, ApplyQuantumGateToQubits) {
 TEST(CircuitTest, ApplyWrongNQubitsQuantumGateToQubits) {
     Solace::QuantumCircuit qc;
     auto H { qc.addQuantumGate(Solace::Gate::Swap()) };
-    auto tMaybe { qc.getGates().at(H).viewTransformer() };
+    auto tMaybe { qc.getGates().at(H).getTransformer() };
     //auto t { std::get<Solace::QuantumGateTransformer>(tMaybe) };
 
     // Create a single qubit on the circuit.
