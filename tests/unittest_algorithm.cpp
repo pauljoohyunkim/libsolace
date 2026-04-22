9d628bc851c9ec5f55c4acfc22cc5c59e70be5d8
김주현 <paulkim001@autocrypt.io>
2026-04-22 13:17:55 +0900

    Nontrivial QPE test

--- tests/unittest_algorithm.cpp
+++ tests/unittest_algorithm.cpp
@@ -24,11 +24,21 @@ TEST(Algorithm, QPE2) {
 }
 
 TEST(Algorithm, QPE3) {
-    Solace::Gate::PhaseShift P { M_PI/4 };
+    Solace::Gate::PhaseShift P { M_PI_4 };
     Solace::Qubits q { 0, 1 };
 
     const auto estimation { Solace::Algorithm::quantum_phase_estimation(P, q, 5) };
 
     // 1 = 0b001
     ASSERT_EQ(estimation, 0b00100);
+}
+
+TEST(Algorithm, QPE4) {
+    Solace::Gate::PhaseShift P { M_PI_2 + M_PI_4 };
+    Solace::Qubits q { 0, 1 };
+
+    const auto estimation { Solace::Algorithm::quantum_phase_estimation(P, q, 5) };
+
+    // 1 = 0b001
+    ASSERT_EQ(estimation, 0b01100);
 }
